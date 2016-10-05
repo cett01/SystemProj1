73eb82f55b29bccc920376aa858bbee78a2d1030
ktyhek <ktyhek@users.noreply.github.com>
2016-10-05 23:27:58 +0300

    Beceriksizler :))
    Bugün bitti bitecekti tam o sırada biz bıraktık :))

--- part1.c
+++ part1.c
@@ -1,64 +1,91 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
+   
 int seq[]= {0,0,0,1,1,1,1,0};
-int gen_count = 15;
-char start[]= {'0','1','1','0','0','1'};
+//int gen_count = 15;
+//char start[]= {'0','1','1','0','0','1'};
 
-
-	
+int assemblySubroutine(int rule, int left, int center, int right);
+ 
+/*
 int next(char * coming){
 		int a=(coming[0]-'0')*4;
 		int b=(coming[1]-'0')*2;
 		int c=(coming[2]-'0')*1;
 	return seq[7-(a+b+c)];
 }
-int main(){
+*/
+
+int main(int argc, char **argv)
+{
+ 
 	
+	FILE *fp;
+	int *line;
+	int *tempLine;
+	int length;
+	int rule;
 	
-	int len_of_start=sizeof(start)/sizeof(start[0]);
-	int column=2*(gen_count-1)+len_of_start;
-	int row=gen_count+1;
-	char matrix[row][column];
-	int i,j;
+	printf("Please input the rule number:\n");
+	scanf("%d", &rule);
+	
+    fp = fopen("input1d.txt", "r+");
+	if(!fp)
+	{
+		printf("ERROR!! Opening");
+	}
+	else
+	{
+		fscanf(fp, "%d", &length);
+		//printf("%d", length);
+		line = (int *)malloc(length*sizeof(int));
+		tempLine = (int *)malloc(length*sizeof(int));
+
+		int i=0;
+		while(!feof(fp)){
+			fscanf(fp, "%d", &line[i]);
+			//printf("%c", line[i]);
+			i++;
+		}
+			
+	}
+    fclose(fp);
+	
+	//printf("\n%s\n", line);
+	int i, j;
+	for(i=0; i<length; i++){
+		printf("%d ", line[i]);
+	}
+    
+	int row=length/2+1;
+	int sayi = assemblySubroutine(rule, 0, 0, 1);
+	printf("\n%d\n", sayi);
+
+/*
 	for(i=0;i<row;i++){
-		for(j=0;j<column;j++){
-			matrix[i][j]='0';	
+		for(j=0; j<length; j++){
+			if(j==0){
+				tempLine[j] = assemblySubroutine(rule, 0, line[j], line[j+1]);	//left control
+
+			}
+			else if(j==length-1){
+				tempLine[j] = assemblySubroutine(rule, line[j-1], line[j], 0);  //right control
+
+			}
+			else{
+				tempLine[j] = assemblySubroutine(rule, line[j-1], line[j], line[j+1]);  //others
+			}
+		//printf("%s\n", line);
+		}
+		
+		for(k=0; k<length; k++){
+			line[k] = tempLine[k];
 		}
 	}
 		
-	i=0;
-	for(j=gen_count-1;j<gen_count+len_of_start-1;j++){
-		matrix[0][j]=start[i++];	
-	}
-	
-
-	
-	for(i=0;i<row-1;i++){
-		for(j=0;j<column;j++){
-			char temp[3];
-			temp[0]=matrix[i][j-1];
-			temp[1]=matrix[i][j];
-			temp[2]=matrix[i][j+1];
-			if(j==0)
-				temp[0]='0';
-			else if( j==column-1)
-				temp[2]='0';
-			int xx=next(temp);
-			if(xx==1)
-				matrix[i+1][j]='1';
-			
-			
-		}	
-	}
-
-	for(i=0;i<row;i++){
-		for(j=0;j<column;j++){
-			printf("%c ",matrix[i][j]);	
-		}
-		printf("\n");
-	}
-	
+	*/
 	return 0;
 
 }
